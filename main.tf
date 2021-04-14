daecf0e61b0aceccdbb8ca781339244181145ca8
Piers Barber <Kochab.96@gmail.com>
2021-04-14 11:52:37 +0100

    example BAE terraform

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "eu-west-2"
+    access_key = var.access_key
+    secret_key = var.secret_key
+}
+
+module "vpc" {
+    source = "./vpc"
+}
+
+module "subnets" {
+    source = "./subnets"
+
+    vpc_id = module.vpc.vpc_id
+    route_id = module.vpc.route_id
+    security_id = module.vpc.security_id
+}
